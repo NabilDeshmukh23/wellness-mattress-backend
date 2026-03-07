4ad42fff451c01d6df1b9c8ad6ea8819eb762212
nabildeshmukh23 <nabild21it@student.mes.ac.in>
2026-03-07 08:27:52 +0530

    ..

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
+// Route Imports
 const authRoutes = require('./routes/auth');
 const profileRoutes = require('./routes/profileRoutes');
 const productRoutes = require('./routes/productRoutes');
@@ -12,21 +13,11 @@ const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
 
-const allowedOrigins = [
-    'https://wellness-admin-panel.onrender.com' // Your production admin panel
-];
 
 app.use(cors({
-    origin: function (origin, callback) {
-      
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: true, 
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
@@ -55,6 +46,7 @@ mongoose.connect(uri, {
 app.get('/', (req, res) => {
     res.send("Wellness Mattress API is Live and Database is Connected!");
 });
+
 
 const PORT = Number(process.env.PORT) || 5000;
 app.listen(PORT, '0.0.0.0', () => { 
