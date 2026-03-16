c63dbcfe984008bb5d861a8a390e97378c5cc23b
nabildeshmukh23 <nabild21it@student.mes.ac.in>
2026-03-16 23:29:24 +0530

    ..

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createOrder, verifyPayment, getAdminDashboardStats } = require('../controllers/orderController');
+const { createOrder, verifyPayment, getAdminDashboardStats, updateOrderStatus } = require('../controllers/orderController');
 const { protect, adminOnly } = require('../middleware/authMiddleware');
 
 // Protect ensures the order is linked to the logged-in user
 router.post('/create', protect, createOrder);
 router.post('/verify', protect, verifyPayment);
-router.put('/admin/order/:id', isAuthenticatedUser, authorizeRoles('Admin'), updateOrderStatus);
+router.put('/admin/order/:id', protect, adminOnly, updateOrderStatus);
 router.get('/admin/dashboard', protect, adminOnly, getAdminDashboardStats);
 
 module.exports = router;
